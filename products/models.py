ab38d82edffa93f23db65f67e3e83a6f9ee1ef08
yayo1983 <yazanenator@gmail.com>
2024-06-25 23:34:10 -0600

    add id type as uuid

--- products/models.py
+++ products/models.py
@@ -1,7 +1,8 @@
-# api/models.py
+import uuid
 from django.db import models
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     sku = models.CharField(max_length=100, unique=True)
     name = models.CharField(max_length=255)
     stock = models.PositiveIntegerField(default=100)
